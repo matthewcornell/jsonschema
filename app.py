f802904974950120226bd70d7af3bbb936fe5c65
matthewcornell <matt@matthewcornell.org>
2020-02-20 14:42:32 -0500

    first pass working

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import json
+from pathlib import Path
+
 import click
+from jsonschema import validate
 
 
 @click.command()
@@ -7,7 +11,13 @@ def app():
     App that plays with using https://python-jsonschema.readthedocs.io to validate a) zoltar3 project
     definition json files, and b) forecast json files.
     """
-    pass
+    with open(Path('inferred-schema.json')) as schema_fp, \
+            open(Path('docs-project.json')) as docs_instance_fp:
+        schema_dict = json.load(schema_fp)
+        docs_instance_dict = json.load(docs_instance_fp)
+    click.echo("validating...")
+    validate(instance=docs_instance_dict, schema=schema_dict)
+    click.echo("valid!")
 
 
 if __name__ == '__main__':
